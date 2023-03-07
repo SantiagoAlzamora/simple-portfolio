4b5e821b186b1870b7ad1df4ccef5432710fa551
Santiago Alzamora <santialzamora_6@hotmail.com>
2023-03-07 14:47:42 -0300

    solve CV link problem

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import "./header.css"
 import Logo from '../../assets/LogoMakr-1vTOgO.png'
+import CV from "../../assets/Santiago-Alzamora-CV-EN.pdf"
 
 export default function Header() {
   return (
@@ -11,7 +12,7 @@ export default function Header() {
         <a href="./#about" className="header-link">About me</a>
         <a href="./#projects" className="header-link">Projects</a>
         <a href="./#contact" className="header-link">Contact me</a>
-        <a href="/src/assets/Santiago-Alzamora-CV-EN.pdf" target={"_blank"} className="resume">Resume</a>
+        <a href={CV} target={"_blank"} className="resume">Resume</a>
       </div>
     </header>
   )
